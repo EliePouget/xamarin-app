1a7b45cc3a24d1e6763185f8137958be32da0afc
rome0026 <kenael.romero@etudiant.univ-reims.fr>
2023-02-01 17:11:41 +0100

    Implémentation du calcul de la taxe

--- TP2_Taxe/TP2_Taxe/TP2_Taxe/MainPage.xaml.cs
+++ TP2_Taxe/TP2_Taxe/TP2_Taxe/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -13,6 +14,35 @@ namespace TP2_Taxe
         public MainPage()
         {
             InitializeComponent();
+        }
+        private void Calculer()
+        {
+            Regex exp = new Regex(@"^\d{1,9}(,\d{1,2})?$");
+            if (entPrix.Text == null || exp.IsMatch(entPrix.Text))
+            {
+                if (swTaxeIncluse.IsToggled)
+                {
+                    lblTaxe.Text = Math.Round((int)slTauxTaxe.Value * int.Parse(entPrix.Text) / (1+ slTauxTaxe.Value),2).ToString();
+                    lblTotal.Text = entPrix.Text;
+                }
+                else
+                {
+                    lblTaxe.Text = ((int)slTauxTaxe.Value * int.Parse(entPrix.Text)).ToString();
+                    lblTotal.Text = ((int)slTauxTaxe.Value * int.Parse(entPrix.Text) + (int)slTauxTaxe.Value).ToString();
+    
+                }
+            }
+            else
+            {
+                lblTaxe.Text = "---";
+                lblTotal.Text = "---";
+            }
+            
+        }
+
+        private void entPrix_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Calculer();
         }
     }
 }
