27051da65e265dd57a3b5aa2a3a90d1b11443e77
ELIE POUGET <elie.pouget@etudiant.univ-reims.fr>
2023-02-15 16:44:28 +0100

    Mise en place des citations : non testé

---
+++ TP3_Fichier/TP3_Fichier/Data/Citation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP3_Fichier.Data
+{
+    public class Citation
+    {
+        public string Texte { get; set; }
+        public string Auteur { get; set; }
+
+    }
+}
